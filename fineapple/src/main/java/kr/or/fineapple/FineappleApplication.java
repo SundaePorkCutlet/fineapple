850ca78992ccd0333edd94a72ef76d2eb2a31529
csh <csh@DESKTOP-HL9TOKK>
2021-12-17 18:45:30 +0900

    12-17 18:45 천석희

--- fineapple/src/main/java/kr/or/fineapple/FineappleApplication.java
+++ fineapple/src/main/java/kr/or/fineapple/FineappleApplication.java
@@ -11,5 +11,4 @@ public class FineappleApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(FineappleApplication.class, args);
 	}
-
 }
--- fineapple/src/test/java/kr/or/fineapple/FineappleApplicationTests.java
+++ fineapple/src/test/java/kr/or/fineapple/FineappleApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class FineappleApplicationTests {
 
+	
 	@Test
 	void contextLoads() {
 	}
-
 }
