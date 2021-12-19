10d0279e2187cd5b564ce7de5323152ba55614e4
홍 <@DESKTOP-5AGTDL3>
2021-12-19 14:41:12 +0900

    12/19 14:41 홍준호 중간 commit

--- fineapple/src/main/java/kr/or/fineapple/service/diet/impl/DietServiceImpl.java
+++ fineapple/src/main/java/kr/or/fineapple/service/diet/impl/DietServiceImpl.java
@@ -1,13 +1,23 @@
 package kr.or.fineapple.service.diet.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 import kr.or.fineapple.domain.IntakeRecord;
+import kr.or.fineapple.service.diet.DietDao;
 import kr.or.fineapple.service.diet.DietService;
 
 @Service("DietServiceImpl")
 public class DietServiceImpl implements DietService{
 
+	@Autowired
+	@Qualifier("dietDaoImpl")
+	private DietDao dietDao;
+	public void setDietDao(DietDao dietDao) {
+		this.dietDao = dietDao;
+	}
+	
 	@Override
 	public void addDietService(IntakeRecord record) throws Exception {
 			
