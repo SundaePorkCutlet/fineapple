f2e640af52422f714df878027a9fc9f5d639454a
csh <csh@DESKTOP-HL9TOKK>
2021-12-24 10:19:13 +0900

    12-24 10:18 addDailyBurnning 화면 수정중

--- fineapple/src/main/java/kr/or/fineapple/service/exer/ExerService.java
+++ fineapple/src/main/java/kr/or/fineapple/service/exer/ExerService.java
@@ -12,15 +12,13 @@ public interface ExerService {
 	
 	public void getUserService(ExerService service)throws Exception;
 	
-	public void addDailyBurnning(BurnningRecord record)throws Exception;
 	
 	public void updateServiceTrgt(ExerService service)throws Exception;
     
 	public void updateBodyInfo(ExerService serivce)throws Exception;
 	
 	public Map<String ,Object> getExerList(Search search)throws Exception;
-	
-	
+		
 	public void addExer(Exer exer)throws Exception;
 	
 	public Exer getExer(int exerNo)throws Exception;
@@ -28,6 +26,8 @@ public interface ExerService {
 	public int postUpdateExer(Exer exer)throws Exception;
 	
 	public int deleteExer(int exerNo)throws Exception;
+	
+	public void addDailyBurnning(BurnningRecord record)throws Exception;
 	
 	
 	public void updateUserService(ExerService serivce)throws Exception;
