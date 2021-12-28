4fad2859459239edd03ed43181ded0a505686c3e
wlsgh <wlsgh@LAPTOP-HEI3FQU0>
2021-12-28 17:23:16 +0900

    2021-12-28 17:23 커뮤니티

---
+++ fineapple/src/main/java/kr/or/fineapple/community/CommunityRestController.java
@@ -0,0 +1,19 @@
+package kr.or.fineapple.community;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/community/json/*")
+public class CommunityRestController {
+	
+	@RequestMapping(value = "updatePostLike", method = RequestMethod.POST)
+	public String updatePostLike() {
+		
+		return null;
+	}
+	
+	
+}
