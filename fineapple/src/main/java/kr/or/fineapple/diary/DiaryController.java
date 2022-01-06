826c749d11cdc2c2979cf40e1b83db4efdad35d0
하리니 <@LAPTOP-BJFFN79I>
2022-01-06 20:45:25 +0900

    Commit 2022/01/06/20:44 하리니
    getUserBodyInfo 완성 초읽기,,진짜 쪼금남앗음
    ㅠ
    ㅠ
    ㅠㅠㅠ화이팅

--- fineapple/src/main/java/kr/or/fineapple/diary/DiaryController.java
+++ fineapple/src/main/java/kr/or/fineapple/diary/DiaryController.java
@@ -1,14 +1,30 @@
 package kr.or.fineapple.diary;
 
+import java.time.Duration;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.YearMonth;
+import java.util.List;
 
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import kr.or.fineapple.domain.User;
+import kr.or.fineapple.domain.common.ViewDuration;
+import kr.or.fineapple.service.diary.DiaryService;
 
 @Controller
 @RequestMapping("/diary/*")
 public class DiaryController {
+	
+	@Autowired
+	@Qualifier("diaryServiceImpl")
+	private DiaryService diaryService;
 	
 	@RequestMapping(value="getDiary")
 	public String getDiary() {
@@ -20,8 +36,28 @@ public class DiaryController {
 	}
 	
 	@RequestMapping(value="getUserBodyInfo")
-	public String getUserBodyInfo() {
-		return "diary/getUserBodyInfo.html";
+	public ModelAndView getUserBodyInfo(HttpSession session) {
+		
+		System.out.println("diary/getUserBodyInfo");
+		
+		////사용자 신체 변화 정보 입력 출력를 위한 parameter
+		//session 내 userId와 오늘 일자 기준 주간 기록 조회
+		String userId = ((User)session.getAttribute("user")).getUserId();
+		LocalDateTime endDate = LocalDate.now().atStartOfDay();
+		LocalDateTime startDate = endDate.minus(Duration.ofDays(6));
+		//viewDuration 내 userId, startDate, endDate, date 셋팅
+		ViewDuration viewDuration = new ViewDuration();
+		viewDuration.setUserId(userId);
+		viewDuration.setEndDate(LocalDate.now());
+		viewDuration.setStartDate(startDate.toLocalDate());
+		System.out.println(viewDuration.getStartDate());
+		
+		List<Object> list = diaryService.getUserBodyInfoList(viewDuration);
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName("diary/getUserBodyInfo.html");
+		mav.addObject("userBodyInfo", list);
+		System.out.println(list);
+		return mav;
 	}
 
 }
