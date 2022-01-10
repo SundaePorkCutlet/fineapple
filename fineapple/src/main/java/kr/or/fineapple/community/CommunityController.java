5e6e9b2924e80e7c47ff5c5402f75a45c04b49f1
sec <sec@LAPTOP-N2PERQI5>
2022-01-10 21:27:01 +0900

    01.10 21:26 현정

--- fineapple/src/main/java/kr/or/fineapple/community/CommunityController.java
+++ fineapple/src/main/java/kr/or/fineapple/community/CommunityController.java
@@ -29,6 +29,7 @@ import kr.or.fineapple.domain.common.Search;
 import kr.or.fineapple.domain.community.Board;
 import kr.or.fineapple.domain.community.Group;
 import kr.or.fineapple.domain.community.GroupUser;
+import kr.or.fineapple.domain.community.MtmQna;
 import kr.or.fineapple.domain.community.Report;
 import kr.or.fineapple.service.community.CommunityService;
 import oracle.net.aso.m;
@@ -362,6 +363,23 @@ public class CommunityController {
 		return "community/getGroupToUserInter.html";
 	}
 	
+	@RequestMapping(value="faq/result")
+	public ModelAndView faqList() throws Exception{
+		List<MtmQna> list = communityService.getFaqList(4);
+		
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName("community/faq.html");
+		mav.addObject("list",list);
+		
+		return mav;
+	}
+	
+	@RequestMapping(value="faq")
+	public String faq()throws Exception{
+		return "community/faq.html";
+	}
+	
+
 	
 }
 
