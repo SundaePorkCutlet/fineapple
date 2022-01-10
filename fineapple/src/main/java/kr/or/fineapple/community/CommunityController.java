a3f2ef353517782f5816f06001b1b03bce9aa689
홍 <@DESKTOP-5AGTDL3>
2022-01-10 21:22:28 +0900

    1/10 21:22 홍준호

--- fineapple/src/main/java/kr/or/fineapple/community/CommunityController.java
+++ fineapple/src/main/java/kr/or/fineapple/community/CommunityController.java
@@ -31,7 +31,6 @@ import kr.or.fineapple.domain.community.Group;
 import kr.or.fineapple.domain.community.GroupUser;
 import kr.or.fineapple.domain.community.Report;
 import kr.or.fineapple.service.community.CommunityService;
-import oracle.net.aso.m;
 
 @Controller
 @RequestMapping("/community/*")
