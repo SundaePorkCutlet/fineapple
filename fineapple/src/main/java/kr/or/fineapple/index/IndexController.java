e0ce877ae585ee26cb2e44d58c900b90126eaeb0
홍 <@DESKTOP-5AGTDL3>
2022-01-13 14:33:11 +0900

    1/13 14:43 홍준호 NavName 설정

--- fineapple/src/main/java/kr/or/fineapple/index/IndexController.java
+++ fineapple/src/main/java/kr/or/fineapple/index/IndexController.java
@@ -1,14 +1,19 @@
 package kr.or.fineapple.index;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class IndexController {
 
     @GetMapping("/")
-    public String index(){
+    public String index(Model model){
      System.out.println("index");
+     
+     model.addAttribute("NavName1","Home");
+     model.addAttribute("NavName2","Main");
+     
     	return "index/index.html";
     }
     
